3558e2ada67de8fddbfa706a4d7096659d3d159f
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-29 18:15:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -179,6 +179,7 @@ def player_numbers(name)
     if name == team_stats[:team_name]
       team_stats.map do |team_data, value|
         team_stats[:players]
+        binding.pry
       end
     end 
   end
